0c37fe9a7320222e899434b4ef39f42d4c9be4ea
DenisZaitsev <zayczev2012@mail.ru>
2022-06-09 18:28:18 +0500

    work06_01router

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,11 @@
 /* eslint-disable max-len */
 <template>
-  <component :is="currentPageComponent" :page-params="curentPageParams"/>
+  <router-view/>
 </template>
 
 <script>
-import MainPage from './pages/MainPage.vue';
-import ProductPage from './pages/ProductPage.vue';
-import NotFoundPage from './pages/NotFoundPage.vue';
-import eventBus from './eventBus';
-
-const routes = {
-  main: 'MainPage',
-  product: 'ProductPage',
-};
 
 export default {
-  data() {
-    return {
-      currentPage: 'main',
-      curentPageParams: {},
-    };
-  },
-  methods: {
-    gotoPage(pageName, pageParams) {
-      this.currentPage = pageName;
-      this.curentPageParams = pageParams || {};
-    },
-  },
-  components: {
-    MainPage, ProductPage, NotFoundPage,
-  },
-  created() {
-    eventBus.$on('gotoPage', (pageName, pageParams) => this.gotoPage(pageName, pageParams));
-  },
-  computed: {
-    currentPageComponent() {
-      return routes[this.currentPage] || 'NotFounPage';
-    },
-  },
+
 };
 </script>
