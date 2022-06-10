0fff34689b72ca23af3747d04f1556c2d30065cb
DenisZaitsev <zayczev2012@mail.ru>
2022-06-10 23:33:36 +0430

    work05_02addVUEX

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import VueRouter from 'vue-router';
 import MainPage from '@/pages/MainPage.vue';
 import NotFoundPage from '@/pages/NotFoundPage.vue';
 import ProductPage from '@/pages/ProductPage.vue';
+import cartPage from '@/pages/CartPages.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   { name: 'main', component: MainPage, path: '/' },
   { name: 'product', component: ProductPage, path: '/product/:id' },
+  { name: 'cart', component: cartPage, path: '/basket' },
   { name: 'notFound', component: NotFoundPage, path: '*' },
 ];
 
